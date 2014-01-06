284f6cc64561866dc37a04384a3be1d9753d85bb
Kyle Derr <kyle.derr@civitaslearning.com>
2014-01-05 18:35:03 -0600

    Add the yamlicious script

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
+import os
 
 setup(
   name='yamlicious',
   packages=find_packages(),
+  scripts=[os.path.join('bin', p) for p in os.listdir('bin')],
 )
